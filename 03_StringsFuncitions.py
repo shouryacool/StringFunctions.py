ac9c8c0b02540a35d507deb944620a8e97316db9
shouryacool <84572733+shouryacool@users.noreply.github.com>
2021-10-15 10:17:51 +0530

    Add files via upload

---
+++ 03_StringsFuncitions.py
@@ -0,0 +1,11 @@
+
+
+
+Story="hello"
+# String function 
+print(len(Story))          #len() function 
+print(Story.endswith("o"))     #endswith(“rry”)
+print(Story.count("e"))         # count(“c”)
+print(Story.capitalize() )    #capitalize()
+print(Story.find("hello"))         # find(word)
+print(Story.replace("hello", "get"))       #replace(oldword, newword)
